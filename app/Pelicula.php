f0102b1031bbb459d9e8fa42f26be6e62f0b821b
Sebastian Peña <jspena1843@misena.edu.co>
2020-06-01 16:34:56 -0500

    actualizar por si las moscas

--- app/Pelicula.php
+++ app/Pelicula.php
@@ -9,4 +9,10 @@ class Pelicula extends Model
     protected $table="film";
     protected $primaryKey ="film_id";
     public $timestamps = false;
+    public function categorias(){       
+    return $this->belongsToMany('App\Categoria',
+                                'film_category',
+                                'film_id',
+                                'category_id');
+}
 }
