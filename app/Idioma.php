e677ea287cc31c701ab373982cc952911874b8e7
Sebastian Peña <jspena1843@misena.edu.co>
2020-06-01 16:55:38 -0500

    por si las moscas otra vez ptmd

--- app/Idioma.php
+++ app/Idioma.php
@@ -3,8 +3,16 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Idioma extends Model
 {
-    //
+    protected $table="language";
+    protected $primaryKey ="language_id";
+    public $timestamps = false;
+    public function peliculas(){
+        return $this->HasMany("App\Pelicula","language_id");
+
+    }
+
 }
